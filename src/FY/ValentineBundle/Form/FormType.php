b0ffc52354c74e40218a020bd0e144435f5a2da7
christianJCK <jecko.christian@gmail.com>
2017-02-07 15:39:12 +0100

    added form type

--- src/FY/ValentineBundle/Form/FormType.php
+++ src/FY/ValentineBundle/Form/FormType.php
@@ -9,10 +9,34 @@
 namespace FY\ValentineBundle\Form;
 
 
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FormType extends AbstractType
 {
 
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class)
+            ->add('lastName', TextType::class)
+            ->add('authorEmail', EmailType::class)
+            ->add('authorPhone', NumberType::class)
+            ->add('city', TextType::class)
+            ->add('comment', TextareaType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'    =>  Form::class
+        ));
+    }
 
 }
