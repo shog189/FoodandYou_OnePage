3dfab64c87e2d4ac63863365702dd7685cff8ef9
christianJCK <jecko.christian@gmail.com>
2017-02-07 15:11:07 +0100

    form initialization

---
+++ src/FY/ValentineBundle/Form/FormType.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: criss
+ * Date: 07/02/17
+ * Time: 15:10
+ */
+
+namespace FY\ValentineBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+
+class FormType extends AbstractType
+{
+
+
+}
